1eaafbfaf030ee48acf5c1c4bbbfcf52103eefe2
lê tuấn <tuan.le@codegym.vn>
2022-10-11 05:01:40 +0700

    Tags in php

---
+++ src/basic-syntax/tags.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php echo 'if you want to serve PHP code in XHTML or XML documents,
+                use these tags'; ?>
+</body>
+</html>
